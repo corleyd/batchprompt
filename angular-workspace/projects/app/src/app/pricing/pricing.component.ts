9adf612b3ef7f208dd5fe438ca10396527b921e0
David Corley <david_corley@hotmail.com>
2025-06-10 09:07:07 -0400

    feat: Update About Us and Pricing pages for clarity, add Contact Us link in toolbar

--- angular-workspace/projects/app/src/app/pricing/pricing.component.ts
+++ angular-workspace/projects/app/src/app/pricing/pricing.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-pricing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.scss']
 })
 export class PricingComponent {
 
+  constructor(
+    public auth: AuthService,
+    private router: Router
+  ) {}
+
+  navigateToDashboard() {
+    this.router.navigate(['/dashboard/home']);
+  }
+
+  joinWaitlist() {
+    this.router.navigate(['/request-access']);
+  }
 }
